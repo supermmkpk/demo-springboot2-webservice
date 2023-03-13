2850e41450ecdf9c5ff1bb54247f058a5465cf2b
supermmkpk <supermmkpk@gmail.com>
2023-03-14 04:04:51 +0900

    JPA Test

---
+++ src/main/java/com/demo/springboot/webservice/domain/posts/PostsRepository.java
@@ -0,0 +1,5 @@
+package com.demo.springboot.webservice.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
