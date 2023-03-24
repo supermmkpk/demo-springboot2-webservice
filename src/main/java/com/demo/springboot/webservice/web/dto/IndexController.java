d33c3f13e419e3525c5dfe61688bed3dfe8238ff
supermmkpk <supermmkpk@gmail.com>
2023-03-24 15:59:29 +0900

    3/24 화면 구현 완료

--- src/main/java/com/demo/springboot/webservice/web/dto/IndexController.java
+++ src/main/java/com/demo/springboot/webservice/web/dto/IndexController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,5 +22,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
